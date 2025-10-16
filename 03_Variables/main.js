aadb400ff4875b692709af28d499c65db5a42214
MuhaddisRehman <96332700+MuhaddisRehman@users.noreply.github.com>
2025-10-16 21:55:10 +0500

    variables

---
+++ 03_Variables/main.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "test_name@gmail.com"
+var accountPassword = "122345"
+accountCity = "city"
+
+// accountId = 2   //not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "12345"
+accountCity = "city_updated"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
